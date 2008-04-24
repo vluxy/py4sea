0f57d018be360c94af254e7c007dd0bdd0dc1162
jundaliu <jundaliu@5f879f80-8f4b-0410-bdb6-4f53e344ee92>
2008-04-24 07:12:43 +0000

    bare version

--- trunk/4sea.py
+++ trunk/4sea.py
@@ -4,11 +4,36 @@
 
 import sys,os
 import webbrowser
+import SocketServer, BaseHTTPServer
 
+def pub(name, txt):
+    """call avahi to publish our svc, based on name and other info"""
+    (pin, pout) = os.popen2("avahi-publish -s "+name+" _4sea._udp 43210 "+txt)
+    print pout.read()
+
+def getsvc():
+    """call avahi browse and return 4sea svc list"""
+    (pin, pout) = os.popen2("avahi-browse _4sea._udp")
+    print pout.read()
+
+class myHTTPhandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_error(404, "File not found")
+        pass
+    def do_POST(self):
+        pass
+
+class myHTTPserver(BaseHTTPServer.HTTPServer): pass
 
 def main():
-	pass
-
+    pub("myself test", "this is txt")
+    getsvc()
+    
+    httpd = myHTTPserver(('',8080),myHTTPhandler)
+    thread.start_new_thread(httpd.handle_request,())
+    webbrowser.open_new("http://localhost:8080")
+    
+    #pass
 
 if __name__ == '__main__':
-	main()
+    main()
